f0f843278635bc1af0972db88a7211d62b650ede
Tobias Poeschl <t.poeschl@poeschl-ldk.de>
2022-07-29 17:11:03 +0200

    test

---
+++ vendor/composer/installed.php
@@ -0,0 +1,24 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-main',
+    'version' => 'dev-main',
+    'aliases' => 
+    array (
+    ),
+    'reference' => '63692355dc01fa320f4df0b0306eaaaecf1bb287',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-main',
+      'version' => 'dev-main',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '63692355dc01fa320f4df0b0306eaaaecf1bb287',
+    ),
+  ),
+);
