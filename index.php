c5919b53d64beee7c858077494d3efc0bc8bd522
Tobias Poeschl <t.poeschl@poeschl-ldk.de>
2022-07-29 17:24:10 +0200

    fixes

--- index.php
+++ index.php
@@ -1,15 +1,6 @@
-
 <?php
 require __DIR__ . '/vendor/autoload.php';
 
-use Cowsayphp\Farm;
-
-header('Content-Type: text/plain');
-
-$text = "Set a message by adding ?message=<message here> to the URL";
-if(isset($_GET['message']) && $_GET['message'] != '') {
-	$text = htmlspecialchars($_GET['message']);
-}
-
-$cow = Farm::create(\Cowsayphp\Farm\Cow::class);
-echo $cow->say($text);
+echo "::test";
+echo $_SERVER['DO_Connecting-IP'];
+echo "test::";
